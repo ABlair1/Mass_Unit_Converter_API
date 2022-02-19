35eca58b461aab11ad277de1f8cbad05cd4996b0
Andrew Blair <61394328+ABlair1@users.noreply.github.com>
2022-02-18 23:35:42 -0500

    Removed debug parameter from app.run() in app.py

--- mass_converter/app.py
+++ mass_converter/app.py
@@ -61,4 +61,4 @@ class MassConverter(Resource):
 api.add_resource(MassConverter, '/<mass>/<units>/<result_units>/')
 
 if __name__ == "__main__":
-    app.run(port=7000, debug=True)
+    app.run(port=7000)
